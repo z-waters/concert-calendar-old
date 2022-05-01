9b01ba84d0d7ca8447fa0958136a324ff2e5f49e
BunsoMendoza <bunsomendoza@gmail.com>
2022-05-01 10:40:01 -0700

    more api data concertList, updated searchbar

--- src/components/ConcertList.js
+++ src/components/ConcertList.js
@@ -1,78 +1,93 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { PaginatedList } from "react-paginated-list";
-
+import axios from "axios";
 
 var concerts = [];
 var pages = 0;
 var current_page = 1;
 var total_entries = 0;
 
-async function getConcerts() {
 
+async function fetchTest(apikey, venueId) {
+  axios.get('https://api.songkick.com/api/3.0/events.json?apikey=' + apikey + '&location=sk:' + venueId)
+    .then(function (response) {
+      var data = response.data;
 
-  let fetchQuery = 'https://api.songkick.com/api/3.0/events.json?apikey=m9qVXGhOvdZmmUQs&location=sk:2846';
+      console.log(data);
+      concerts = data.resultsPage.results.event;
 
-  await fetch(fetchQuery, {
-    //mode: 'no-cors',
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    }
-  })
-    .then(response => {
-      if (response.ok) {
-        
-        return response.json();
-      }
-      throw new Error('Something went wrong');
+      concerts.forEach(item => {
+        if (item.start.time == null) {
+          item.start.time = "N/A";
+        }
+        var temp = ""
+        item.performance.forEach(performer => {
+          temp += performer.displayName;
+          if (item.performance.indexOf(performer) < item.performance.length - 1) {
+            temp += ", ";
+          }
+        });
+        item.performance = temp;
+      });
+      total_entries = data.resultsPage.totalEntries;
+      pages = Math.round(total_entries / 50);
+      current_page = data.resultsPage.page;
+      console.log(concerts);
     })
-    .then(data => {
-      console.log(data)
-      data = JSON.parse(JSON.stringify(data));
-      console.log(data['resultsPage']['results']['event'])
-      concerts = data['resultsPage']['results']['event'];
-      total_entries = data['resultsPage']['totalEntries']; 
-      pages = Math.round(total_entries/ 50);
-      current_page = data['resultsPage']['page'];
-      console.log(pages);
-  })
-  .catch(e => console.log(e));
-
-  return true;
+    .catch(function (error) {
+      console.log(error);
+    })
+}
+function timeCheck(timeString) {
+  if (timeString != null) {
+    return new Date(item.start.datetime).toLocaleTimeString();
+  } else {
+    return "N/A";
+  }
 }
 
-
-
 function ConcertList() {
-  getConcerts();
-  
+  var apikey = "m9qVXGhOvdZmmUQs";
+  var seattleEventId = '2846';
+  fetchTest(apikey, seattleEventId);
+
+
   return (
-    
+   
       <PaginatedList
         list={concerts}
         itemsPerPage={50}
         currentPage={current_page}
         displayRange={7}
-        renderList={(list, displayRange) => (
+        renderList={(list) => (
           <>
             {list.map((item, id) => {
-              
+
               return (
-               
-                <div  key={id} className="list-group-item" id="list-item"> <div><p>{displayRange}</p></div>
-                <Row>
-                  <Col md="4">
-                    {item.displayName}
-                  </Col>
-                  <Col md="4">
-                    {item.location['city']}
-                  </Col>
-                  <Col md="4">
-                    {item.type}
-                  </Col>
-                
-                </Row>
+
+                <div key={id} className="list-group-item" id="list-item">
+                  <Row>
+                    <Col md="3">
+                      {item.displayName.split(" (")[0]}
+                    </Col>
+                    <Col md="2">
+                      {item.location['city'].substring(0, item.location['city'].length - 4)}
+                    </Col>
+                    <Col md="1">
+                      {new Date(item.start.datetime).toLocaleDateString()}
+                    </Col>
+                    <Col md="1">
+                      {new Date(item.start.datetime).toLocaleTimeString()}
+                    </Col>
+                    <Col md="3">
+                      {item.venue.displayName}
+                    </Col>
+                    <Col md="2">
+                      {item.performance}
+                    </Col>
+
+                  </Row>
                 </div>
               );
             })}
@@ -81,10 +96,43 @@ function ConcertList() {
       />
 
 
-    
 
 
   );
 }
 
 export default ConcertList;
+
+
+
+ //metroarea id by name 
+//https://api.songkick.com/api/3.0/search/locations.json?query={input}&apikey=m9qVXGhOvdZmmUQs
+
+///upcoming events by artist name
+//https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={your_api_key}
+
+//event details 
+//https://api.songkick.com/api/3.0/events/{event_id}.json?apikey={your_api_key}
+
+//metroarea upcoming events
+//https://api.songkick.com/api/3.0/metro_areas/{metro_area_id}/calendar.json?apikey={your_api_key}
+
+//get venue id by name
+//https://api.songkick.com/api/3.0/search/venues.json?query={venue_name}&apikey={your_api_key}
+
+//upcoming events by venue
+//https://api.songkick.com/api/3.0/venues/{venue_id}/calendar.json?apikey={your_api_key}
+
+// ageRestriction
+// displayName
+// flaggedAsEnded
+// id
+// location{city,long,lat}
+// performance[]
+// start{data, datetime,time}
+// popularity
+// venue{id,displayName}
+//let fetchQuery = 'https://api.songkick.com/api/3.0/events.json?apikey=m9qVXGhOvdZmmUQs&location=sk:2846';
+
+
+
--- src/components/EventsPage.js
+++ src/components/EventsPage.js
@@ -16,15 +16,24 @@ function EventsPage() {
         </h1>
 
         <div className="list-group-item" id="list-item"> 
-                <Row>
-                  <Col md="4">
+        <Row>
+                  <Col md="3">
                     Event
                   </Col>
-                  <Col md="4">
+                  <Col md="2">
                     Location
                   </Col>
-                  <Col md="4">
-                    Type
+                  <Col md="1">
+                    Date
+                  </Col>
+                  <Col md="1">
+                    Time
+                  </Col>
+                  <Col md="3">
+                    Venue
+                  </Col>
+                  <Col md="2">
+                    Scheduled Performers
                   </Col>
                 
                 </Row>
